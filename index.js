1212d574fff927a4475002e1c75bbaf848e0cf9d
Rodrigo Gorga <108111681+gorga7@users.noreply.github.com>
2023-09-09 12:23:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 
 
-fetch('https://pokeapi.co/api/v2/pokemon/')
-.then(response=>response.json())
-.then (data =>{
-    ul=document.getElementById("listausuarios")
-data.results.forEach(element => {
-    let li = document.createElement ("li");
-    li.innerHTML = element.name + " " + element.url;
-    ul.append(li)
-});
+async function Display (){
+    
+    try{ await fetch('https://pokeapi.co/api/v2/pokemon/')
+    .then(response=>response.json())
+    .then (data =>{
+        ul=document.getElementById("listausuarios")
+        data.results.forEach(element => {
+        let li = document.createElement ("li");
+        li.innerHTML = element.name + " " + element.url;
+        ul.append(li)
+    });
+    
+    })} catch(error){
+        console.log(error)
+    }
+   }
 
-})
+Display()
 
 
 
